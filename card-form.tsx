7ce62f79bddecf01d2a672cda4410fc89fc37027
jitendra19 <jiten.cs2010@gmail.com>
2021-06-21 23:08:03 +0530

    using state, grabed input value

--- card-form.tsx
+++ card-form.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 export default class CardForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: ''
+    };
   }
-  username = React.createRef();
+  // username = React.createRef();
   handleSubmit = event => {
-    console.log('this.username', this.username.current.value);
+    // console.log('this.username', this.username.current.value);
+    console.log('this.username', this.state.username);
     event.preventDefault();
   };
   render() {
@@ -14,9 +18,11 @@ export default class CardForm extends React.Component {
       <form style={{ marginBottom: '2rem' }} onSubmit={this.handleSubmit}>
         <input
           type="text"
+          value={this.state.username}
           placeholder="enter github username"
           required
-          ref={this.username}
+          onChange={event => this.setState({ username: event.target.value })}
+          // ref={this.username}
         />
         <button>Add Card</button>
       </form>
