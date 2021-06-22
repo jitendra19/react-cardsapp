85c9e43f4d9f7f42075fc7bc141db83f0617957c
jitendra19 <jiten.cs2010@gmail.com>
2021-06-22 19:23:23 +0530

    profiles are shifted to parent component, passing to cardlist component

--- cardList.tsx
+++ cardList.tsx
@@ -1,33 +1,16 @@
 import React from 'react';
 import Card from './card';
 
-const testData = [
-  {
-    name: 'Dan Abramov',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4',
-    company: '@facebook'
-  },
-  {
-    name: 'Sophie Alpert',
-    avatar_url: 'https://avatars2.githubusercontent.com/u/6820?v=4',
-    company: 'Humu'
-  },
-  {
-    name: 'Sebastian Markbåge',
-    avatar_url: 'https://avatars2.githubusercontent.com/u/63648?v=4',
-    company: 'Facebook'
-  }
-];
-
 export default class CardList extends React.Component {
   constructor(props) {
     super(props);
   }
   // code
   render() {
+    // const profiles
     return (
       <div>
-        {testData.map(profile => (
+        {this.props.profiles.map(profile => (
           <Card {...profile} />
         ))}
       </div>
--- index.tsx
+++ index.tsx
@@ -10,11 +10,30 @@ interface AppState {
   name: string;
 }
 
+const testData = [
+  {
+    name: 'Dan Abramov',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/810438?v=4',
+    company: '@facebook'
+  },
+  {
+    name: 'Sophie Alpert',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/6820?v=4',
+    company: 'Humu'
+  },
+  {
+    name: 'Sebastian Markbåge',
+    avatar_url: 'https://avatars2.githubusercontent.com/u/63648?v=4',
+    company: 'Facebook'
+  }
+];
+
 class App extends Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'React'
+      name: 'React',
+      profiles: testData
     };
   }
   addUsername = username => {
@@ -25,7 +44,7 @@ class App extends Component<AppProps, AppState> {
       <div>
         <h1>Github Cards App</h1>
         <CardForm addUsername={this.addUsername} />
-        <CardList />
+        <CardList profiles={this.state.profiles} />
       </div>
     );
   }
